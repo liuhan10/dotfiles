0b62a4e42c017e671f99ded65b80582b0dcb21a1
ZhongGaohao <zghpku@gmail.com>
2015-07-14 13:48:49 +0800

    update python version

--- third_party/pyenv.sh
+++ third_party/pyenv.sh
@@ -3,6 +3,6 @@ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 source ~/.zshrc
-pyenv install 2.7.9
+pyenv install 2.7.10
 pyenv rehash
-pyenv global 2.7.9
+pyenv global 2.7.10
